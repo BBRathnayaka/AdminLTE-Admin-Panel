ae472455340e62347997a13b9e27144ad3c8f659
Budditha <90sbudditha@gmail.com>
2020-05-01 23:47:05 +0530

    Setting Admin Pages

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -8,9 +8,32 @@
       </li>
     </ul>
 
+    <!-- SEARCH FORM 
+    <form class="form-inline ml-3">
+      <div class="input-group input-group-sm">
+        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-navbar" type="submit">
+            <i class="fas fa-search"></i>
+          </button>
+        </div>
+      </div>
+    </form>
+    -->
+    
     <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
 
+    <ul class="navbar-nav ml-auto">
+    <form class="form-inline ml-3">
+      <div class="input-group input-group-sm">
+        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-navbar" type="submit">
+            <i class="fas fa-search"></i>
+          </button>
+        </div>
+      </div>
+    </form>
 
       <li class="nav-item">
         <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
